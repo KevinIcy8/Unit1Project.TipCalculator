5db55923175039364dd63309f527e442fedba377
BT_4N2_21 <KevinIcy8>
2022-09-21 12:14:59 -0400

    Initial commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        // user input variables
+        System.out.print("What is the total bill: $ ");
+        float totalBill = s.nextFloat();
+        System.out.print("What tip percentage do you want to give: ");
+        int tipPercentage = s.nextInt();
+        System.out.print("How many people are in the party: ");
+        int numberOfPeople = s.nextInt();
+        // calculation variables
+        float tipPercentageAsDecimal = (float)tipPercentage/100; //doing this makes calculations easier
+        float totalTip = totalBill*tipPercentageAsDecimal;
+        float roundedTotalTip = (float)Math.round(totalTip*100)/100;
+        float totalBillWithTip = totalBill+roundedTotalTip;
+        float tipPerPerson = roundedTotalTip/numberOfPeople;
+        float roundedTipPerPerson = (float)Math.round(tipPerPerson*100)/100;
+        float totalBillPerPerson = totalBillWithTip/numberOfPeople;
+        float roundedBillPerPerson = (float)Math.round(totalBillPerPerson*100/100);
+        // print statements and format
+        System.out.println();
+        System.out.println("Total Tip: $" + roundedTotalTip);
+        System.out.println("Total Bill Including Tip: $" + totalBillWithTip);
+        System.out.println("Tip Per Person: $" + roundedTipPerPerson);
+        System.out.println("Total Bill Per Person: $" + totalBillPerPerson);
+        System.out.println(roundedBillPerPerson);
+
+    }
+}
